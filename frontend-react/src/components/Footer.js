54f40d2a9cf96c9d778f85c552576c2b168ec20c
Romain Toso <romaintoso250@gmail.com>
2026-03-07 04:00:57 +0100

    update: Footer

--- frontend-react/src/components/Footer.js
+++ frontend-react/src/components/Footer.js
@@ -3,14 +3,8 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 UpcycleConnect — Projet Annuel ESGI huihihkh 2A5 ✓</p>
+      <p>© 2026 UpcycleConnect — Projet Annuel ESGI ddddddd 2A5</p>
       <p>Contact : <a href="mailto:contact@upcycleconnect.net">contact@upcycleconnect.net</a></p>
-      <p>GitHub : <a href="https://github.com/Kaazhar/PA_2a5_2026">https://github.com/Kaazhar/PA_2a5_2026</a></p>
-      <p>LinkedIn : <a href="https://www.linkedin.com/in/kaazhar/">https://www.linkedin.com/in/kaazhar/</a></p>
-      <p>Twitter : <a href="https://twitter.com/kaazhar">https://twitter.com/kaazhar</a></p>
-      <p>Facebook : <a href="https://www.facebook.com/kaazhar">https://www.facebook.com/kaazhar</a></p>
-      <p>Instagram : <a href="https://www.instagram.com/kaazhar/">https://www.instagram.com/kaazhar/</a></p>
-      <p>YouTube : <a href="https://www.youtube.com/kaazhar">https://www.youtube.com/kaazhar</a></p>
     </footer>
   );
 }
